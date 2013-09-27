4f9c57a79332b91532583389c730e7d1cedcaec5
Daniel Lauzon <daniel.lauzon@gmail.com>
2013-09-27 15:32:19 -0400

    start styling, addd clock

--- src/js/controllers/home_controller.js
+++ src/js/controllers/home_controller.js
@@ -1,15 +1,24 @@
-angular.module(_CONTROLLERS_).controller('HomeController', function($scope) {
+angular.module(_CONTROLLERS_).controller('HomeController', function($scope,$timeout) {
 
   $scope.text = "iMetrical Swiper";
   $scope.slides = [
-    {text:'One', imgIdx:1},
-    {text:'Two', imgIdx:2},
-    {text:'Three', imgIdx:3}];
+    {caption:'Live', metric:'300s', imgIdx:0},
+    {caption:'Day', metric:'24h', imgIdx:1},
+    {caption:'Month', metric:'30d', imgIdx:2},
+    {caption:'Year', metric:'365d', imgIdx:3}];
+    $scope.stamp = '';
+  $scope.onTimeout = function(){
+    var d = new Date();
+    $scope.stamp = ''+d.getHours()+':'+d.getMinutes()+':'+d.getSeconds();
+    mytimeout = $timeout($scope.onTimeout,1000);
+  }    
+  var mytimeout = $timeout($scope.onTimeout,1000);
 
   setTimeout(function(){
     window.mySwipe = new Swipe(document.getElementById('slider'),{
       auto: 5000,
-      continuous: true
+      continuous: true,
+      startSlide: 1
     });
   },500);
 

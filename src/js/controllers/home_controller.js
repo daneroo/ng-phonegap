38fd486d63670b5682435f5f09c863ab99c81b9e
Daniel Lauzon <daniel.lauzon@gmail.com>
2013-09-27 15:38:31 -0400

    fix for 4s

--- src/js/controllers/home_controller.js
+++ src/js/controllers/home_controller.js
@@ -5,8 +5,9 @@ angular.module(_CONTROLLERS_).controller('HomeController', function($scope,$time
     {caption:'Live', metric:'300s', imgIdx:0},
     {caption:'Day', metric:'24h', imgIdx:1},
     {caption:'Month', metric:'30d', imgIdx:2},
-    {caption:'Year', metric:'365d', imgIdx:3}];
-    $scope.stamp = '';
+    {caption:'Year', metric:'365d', imgIdx:3}
+  ];
+  $scope.stamp = '';
   $scope.onTimeout = function(){
     var d = new Date();
     $scope.stamp = ''+d.getHours()+':'+d.getMinutes()+':'+d.getSeconds();
